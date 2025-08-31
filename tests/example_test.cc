d4bb7df9bc6eef6fedd16676b2e40baed98bb00d
Jayden <cjjunk29@gmail.com>
2025-08-31 16:56:39 +0900

    jayden/add-gtest-gbenchmark (#4)
    * chore: Update .gitignore to include Testing directory for improved test management
    
    * feat: Add initial benchmark implementation in benchmark_main.cpp for performance testing
    
    * test: Add initial unit test for example functionality using Google Test framework
    
    * chore: Add CMake configuration for project setup, including Google Test and Google Benchmark dependencies
    
    * test: Add CMake configuration for Google Test with an example test case
    
    * feat: Add CMake configuration for benchmarking with Google Benchmark integration
    
    * chore: added newline to end of gitignore
    
    * chore: added newline to end of CMakeLists.txt

---
+++ tests/example_test.cc
@@ -0,0 +1,11 @@
+/*
+ *   Copyright 2025 Team Arboris
+ *   Licensed under the Apache License, Version 2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#include <gtest/gtest.h>
+
+TEST(ExampleTest, BasicTest) {
+  EXPECT_EQ(1, 1);
+}
