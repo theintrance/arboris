708ea5148f9a4de0f6342f1c7bb8cb839b5cfa69
Jayden <cjjunk29@gmail.com>
2025-09-01 11:19:21 +0900

    jayden/cpp-code-directory-structure

---
+++ include/arboris/arboris.hpp
@@ -0,0 +1,12 @@
+/*
+ *   Copyright 2025 Team Arboris
+ *   Licensed under the Apache License, Version 2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#ifndef ARBORIS_ARBORIS_HPP_
+#define ARBORIS_ARBORIS_HPP_
+
+// TODO(team): Add public APIs
+
+#endif  // ARBORIS_ARBORIS_HPP_
---
+++ src/string/string.hpp
@@ -0,0 +1,16 @@
+/*
+ *   Copyright 2025 Team Arboris
+ *   Licensed under the Apache License, Version 2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#ifndef SRC_STRING_STRING_HPP_
+#define SRC_STRING_STRING_HPP_
+
+namespace arboris {
+
+// TODO(team): Add string utility's interfaces
+
+}
+
+#endif  // SRC_STRING_STRING_HPP_
