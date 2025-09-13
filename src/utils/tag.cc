789ca8ea956c4d4b3dad37aefa6d7503487436e4
BayernMuller <cjjunk29@gmail.com>
2025-09-13 12:02:20 +0900

    fix: wrong indentation on copyright comment

--- src/utils/tag.cc
+++ src/utils/tag.cc
@@ -1,8 +1,8 @@
 /*
-    *   Copyright 2025 Team Arboris
-    *   Licensed under the Apache License, Version 2.0
-    *   http://www.apache.org/licenses/LICENSE-2.0
-    */
+ *   Copyright 2025 Team Arboris
+ *   Licensed under the Apache License, Version 2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 #include <unordered_map>
 
